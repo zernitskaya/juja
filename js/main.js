faad04b6541c86089e4e66fe75e0ca12c1fac45c
yurch-html-alterpost <yurch-html@alterpost.org>
2020-10-12 22:37:55 +0300

    add modal

--- js/main.js
+++ js/main.js
@@ -27,98 +27,135 @@
     const queryClose = document.querySelector('.js-close-query');
 
     body.addEventListener('click', function (e) {
-        if (isClosest(e.target, menuBtn)) {
-            menu.classList.toggle('active');
-            body.classList.toggle('fixed');
+        // if (isClosest(e.target, menuBtn)) {
+        //     menu.classList.toggle('active');
+        //     body.classList.toggle('fixed');
+        // }
+
+        // if (isClosest(e.target, menuOverlay)) {
+        //     menu.classList.toggle('active');
+        //     body.classList.toggle('fixed');
+        // }
+        //
+        // if (isClosest(e.target, closeMenu)) {
+        //     menu.classList.toggle('active');
+        //     body.classList.toggle('fixed');
+        // }
+        //
+        // if (isClosest(e.target, select)) {
+        //     selectItem.classList.toggle('active');
+        //     body.classList.toggle('fixed');
+        // }
+        //
+        // if (isClosest(e.target, selectOverlay)) {
+        //     selectItem.classList.toggle('active');
+        //     body.classList.toggle('fixed');
+        // }
+        //
+        // if (isClosest(e.target, selectClose)) {
+        //     selectItem.classList.toggle('active');
+        //     body.classList.toggle('fixed');
+        // }
+        //
+        // if (isClosest(e.target, addSkill)) {
+        //     addSkillItem.classList.toggle('active');
+        // }
+        //
+        // if (isClosest(e.target, addverlay)) {
+        //     addSkillItem.classList.toggle('active');
+        // }
+        //
+        // if (isClosest(e.target, addClose)) {
+        //     addSkillItem.classList.toggle('active');
+        // }
+        //
+        // if (isClosest(e.target, mapOpen)) {
+        //     map.classList.toggle('active');
+        //     body.classList.toggle('fixed');
+        // }
+        //
+        // if (isClosest(e.target, mapOverlay)) {
+        //     map.classList.toggle('active');
+        //     body.classList.toggle('fixed');
+        // }
+        //
+        // if (isClosest(e.target, mapClose)) {
+        //     map.classList.toggle('active');
+        //     body.classList.toggle('fixed');
+        // }
+        //
+        // if (isClosest(e.target, queryOpen)) {
+        //     queryItem.classList.toggle('active');
+        //     body.classList.toggle('fixed');
+        // }
+        //
+        // if (isClosest(e.target, queryOverlay)) {
+        //     queryItem.classList.toggle('active');
+        //     body.classList.toggle('fixed');
+        // }
+        //
+        // if (isClosest(e.target, queryClose)) {
+        //     queryItem.classList.toggle('active');
+        //     body.classList.toggle('fixed');
+        // }
+    });
+})();
+
+(function() {
+    const body = document.querySelector('body');
+    const modals = document.querySelectorAll('[data-modal-name]');
+    let count = 0;
+
+    document.addEventListener('click', function(e) {
+        const elemStatus = isClosestSet(e.target, 'data-modal-open');
+        const closeModal = isClosestSet(e.target, 'data-modal-close');
+        if (elemStatus) {
+            for (let i = 0; i < modals.length; i++) {
+                if (elemStatus.dataset.modalOpen === modals[i].dataset.modalName) {
+                    modals[i].classList.add('active');
+                    body.classList.add('fixed');
+
+                    count = count + 1;
+                }
+            }
         }
 
-        if (isClosest(e.target, menuOverlay)) {
-            menu.classList.toggle('active');
-            body.classList.toggle('fixed');
-        }
+        if (closeModal) {
+            for (let i = 0; i < modals.length; i++) {
+                if (closeModal.dataset.modalClose === modals[i].dataset.modalName) {
+                    modals[i].classList.remove('active');
 
-        if (isClosest(e.target, closeMenu)) {
-            menu.classList.toggle('active');
-            body.classList.toggle('fixed');
-        }
+                    count = count - 1;
 
-        if (isClosest(e.target, select)) {
-            selectItem.classList.toggle('active');
-            body.classList.toggle('fixed');
-        }
-
-        if (isClosest(e.target, selectOverlay)) {
-            selectItem.classList.toggle('active');
-            body.classList.toggle('fixed');
-        }
-
-        if (isClosest(e.target, selectClose)) {
-            selectItem.classList.toggle('active');
-            body.classList.toggle('fixed');
-        }
-
-        if (isClosest(e.target, addSkill)) {
-            addSkillItem.classList.toggle('active');
-        }
-
-        if (isClosest(e.target, addverlay)) {
-            addSkillItem.classList.toggle('active');
-        }
-
-        if (isClosest(e.target, addClose)) {
-            addSkillItem.classList.toggle('active');
-        }
-
-        if (isClosest(e.target, mapOpen)) {
-            map.classList.toggle('active');
-            body.classList.toggle('fixed');
-        }
-
-        if (isClosest(e.target, mapOverlay)) {
-            map.classList.toggle('active');
-            body.classList.toggle('fixed');
-        }
-
-        if (isClosest(e.target, mapClose)) {
-            map.classList.toggle('active');
-            body.classList.toggle('fixed');
-        }
-
-        if (isClosest(e.target, queryOpen)) {
-            queryItem.classList.toggle('active');
-            body.classList.toggle('fixed');
-        }
-
-        if (isClosest(e.target, queryOverlay)) {
-            queryItem.classList.toggle('active');
-            body.classList.toggle('fixed');
-        }
-
-        if (isClosest(e.target, queryClose)) {
-            queryItem.classList.toggle('active');
-            body.classList.toggle('fixed');
+                    if (count <= 0) {
+                        body.classList.remove('fixed');
+                    }
+                }
+            }
         }
     });
 })();
 
-
-function isClosest(eventTarget, thisElem) {
+function isClosestSet(eventTarget, thisElem) {
     let result = false;
+
     if (!thisElem || !eventTarget) return false;
 
-    if (eventTarget.className === thisElem.className) {
-        return true;
+    if (eventTarget.getAttribute(thisElem)) {
+        return eventTarget;
     }
 
     for (;true;) {
         eventTarget = eventTarget.parentElement;
 
         if (!eventTarget) {
+            result = false;
             break;
         }
 
-        if (eventTarget.className === thisElem.className) {
-            result = true;
+        if (eventTarget.getAttribute(thisElem)) {
+            result = eventTarget;
+            break;
         }
     }
 
