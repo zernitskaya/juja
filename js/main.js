3876351ff2ee863170e03d7fcea4c1811dc9ef66
yurch-html-alterpost <yurch-html@alterpost.org>
2020-10-14 00:37:49 +0300

    gallery

--- js/main.js
+++ js/main.js
@@ -1,39 +1,124 @@
 (function() {
+   function lightBox(gallery) {
+       const imgWrap = document.querySelector(gallery);
+       const imgLightbox = document.querySelector('.js-lightbox__img');
+       const lightbox = document.querySelector('.js-lightbox');
+       const lightClose = document.querySelector('.js-lightbox-close');
+       const img = '.gallery__img';
+       const itemClass = 'gallery__item';
+       const buttonLeft = document.querySelector('.js-left');
+       const buttonRight = document.querySelector('.js-right');
+       const itemGallery = imgWrap.querySelectorAll('.'+itemClass);
+       let prev;
+       let next;
+
+       imgWrap.addEventListener('click', function(e) {
+           let gallery = isClosest(e.target, itemClass);
+
+           if (gallery) {
+               lightbox.classList.add('active');
+
+               const urlImg = gallery.querySelector(img).getAttribute('src');
+               imgLightbox.setAttribute('src', urlImg);
+           }
+       });
+
+       buttonLeft.addEventListener('click', function() {
+           const urlImg = imgLightbox.getAttribute('src');
+           const arrThisImg = Array.prototype.slice.call(itemGallery);
+
+           const filterItem = arrThisImg.filter(function(item) {
+               return item.querySelector(img).getAttribute('src') === urlImg;
+           });
+
+           newArrfunction(filterItem[0]);
+           if(!prev && !prev.length) {return}
+           imgLightbox.setAttribute('src', prev[0]);
+       });
+
+       buttonRight.addEventListener('click', function() {
+           const urlImg = imgLightbox.getAttribute('src');
+           const arrThisImg = Array.prototype.slice.call(itemGallery);
+
+           const filterItem = arrThisImg.filter(function(item) {
+               return item.querySelector(img).getAttribute('src') === urlImg;
+           });
+
+           newArrfunction(filterItem[0]);
+
+           imgLightbox.setAttribute('src', next[0]);
+       });
+
+       lightClose.addEventListener('click', function() {
+           lightbox.classList.remove('active');
+       });
+
+       function newArrfunction(itemGal) {
+           prev = isSiblingPrev(itemGal,'gallery__item').map( function(item) {
+               return item.querySelector(img).getAttribute('src');
+           });
+
+           next = isSiblingNext(itemGal,'gallery__item').map( function(item) {
+               return item.querySelector(img).getAttribute('src');
+           });
+       }
+   }
+
+    lightBox('.gallery');
+
+    function isSiblingPrev(tag, classItem) {
+        let arr = [];
+        let prev = tag;
+
+        if (!tag) return;
+
+        for (;true;) {
+            if (!prev) {
+                break;
+            }
+
+            prev = prev.previousElementSibling;
+
+            if (prev && prev.classList.contains(classItem)) {
+                arr.push(prev);
+            }
+        }
+
+        return arr;
+    }
+
+    function isSiblingNext(tag, classItem) {
+        let arr = [];
+        let next = tag;
+
+        if (!tag) return;
+
+        for (;true;) {
+            if (!next) {
+                break;
+            }
+
+            next = next.nextElementSibling;
+
+            if (next && next.classList.contains(classItem)) {
+                arr.push(next);
+            }
+        }
+
+        return arr;
+    }
+})();
+(function() {
     const menu = document.querySelector('.js-acordion-wrap');
-    const itemClass = 'js-accordion-item';
+    const itemClass = 'acordion__title';
 
     menu.addEventListener('click', function(e) {
         const el = isClosest(e.target, itemClass);
 
         if(el && el.classList.contains(itemClass)) {
-            el.classList.toggle('active');
+            el.parentElement.classList.toggle('active');
         }
     });
-
-    function isClosest(eventTarget, thisElem) {
-        let result = false;
-        if (!thisElem || !eventTarget) return false;
-
-        if (eventTarget.classList.contains(thisElem)) {
-            return eventTarget;
-        }
-
-        for (;true;) {
-            eventTarget = eventTarget.parentElement;
-
-            if (!eventTarget) {
-                result = false;
-                break;
-            }
-
-            if (eventTarget.classList.contains(thisElem)) {
-                result = true;
-                break;
-            }
-        }
-
-        return eventTarget;
-    }
 })();
 
 (function() {
@@ -95,4 +180,29 @@ function isClosestSet(eventTarget, thisElem) {
     }
 
     return result;
+}
+
+function isClosest(eventTarget, thisElem) {
+    let result = false;
+    if (!thisElem || !eventTarget) return false;
+
+    if (eventTarget.classList.contains(thisElem)) {
+        return eventTarget;
+    }
+
+    for (;true;) {
+        eventTarget = eventTarget.parentElement;
+
+        if (!eventTarget) {
+            result = false;
+            break;
+        }
+
+        if (eventTarget.classList.contains(thisElem)) {
+            result = true;
+            break;
+        }
+    }
+
+    return eventTarget;
 }
